728a8a7a0d23e43b03a22337336b42d19aae52fc
unknown <tarpel.gta@gmail.com>
2024-12-29 01:25:14 +0200

    add: carbon-dating and dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (typeof members !== 'object' || !Boolean(members) || !Array.isArray(members)) return false;
+
+  const filteredMembers = members.filter((item) => typeof item === 'string' && item.trim().length > 0);
+
+  if (filteredMembers.length === 0) return false;
+  return filteredMembers.map((item) => item.trim().at(0).toUpperCase()).sort().join('');
 }
 
 module.exports = {
