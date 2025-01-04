0047922406a01be0164bb24dc0c850f56af0992b
unknown <tarpel.gta@gmail.com>
2025-01-04 10:00:05 +0200

    add: what season solution #2

--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
+
 function getSeason(date) {
-  if (!date ) throw new Error('Unable to determine the time of year!');
+  if (!date) return 'Unable to determine the time of year!';
   
   try {
     date.getTime();
