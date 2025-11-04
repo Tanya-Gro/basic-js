8b5da87d5a90d23e144931c66668b8382cf98bcd
Tanya-Gro <tarpel.gta@gmail.com>
2025-11-04 14:16:52 +0200

    feat: encodeLine solution

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine( str ) {
+  const arr = str.split('');
+  let result = '';
+  let count = 1;
+  
+  for(let i = 1; i < arr.length; i += 1) {
+    if(arr[i] === arr[i-1]) count += 1;
+    else {
+      result += (count === 1) ? arr[i-1] : count.toString() + arr[i-1];
+      count = 1;
+    }
+    if (i === arr.length -1) result += (count === 1) ? arr[i] : count.toString() + arr[i];
+  }
+  return result;
 }
 
 module.exports = {
